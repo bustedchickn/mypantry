a25c2eba49dd27b05b71b0b07db4e3681fbd15cf
bustedchickn <86179078+bustedchickn@users.noreply.github.com>
2025-05-07 11:16:39 -0600

    Update main.dart

--- my_pantry/lib/main.dart
+++ my_pantry/lib/main.dart
@@ -1,42 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-List<Map<String, dynamic>> shoppingList = [];
-
-
-
-
-
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      title: 'Shopping List',
       home: ShoppingListPage(),
     );
   }
@@ -45,86 +15,112 @@ class MyApp extends StatelessWidget {
 class ShoppingListPage extends StatefulWidget {
   @override
   _ShoppingListPageState createState() => _ShoppingListPageState();
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-
 }
 
 
 class _ShoppingListPageState extends State<ShoppingListPage> {
-  int _counter = 0;
+  List<Map<String, dynamic>> shoppingList = [];
+  final FocusNode _ghostFocusNode = FocusNode();
+  final TextEditingController _ghostController = TextEditingController();
+  List<TextEditingController> controllers = [];
 
-  void addShoppingItem(String itemName){
-  shoppingList.add({'item': itemName, 'checked' : false});
-}
+  void addItem(String itemName) {
+    if (itemName.trim().isEmpty) return;
+    setState(() {
+      shoppingList.add({'item': itemName.trim(), 'checked': false});
+      controllers.add(TextEditingController(text:itemName.trim()));
+      _ghostController.clear();
+    });
+  }
 
-void removeShoppingItem(String itemName){
-  shoppingList.removeWhere((item) => item['item'] == itemName);
-}
+  void updateItem(int index, String newText) {
+    shoppingList[index]['item'] = newText;
+    
+  }
 
-void toggleCheck(int index){
-  shoppingList[index]['checked'] = !shoppingList[index]['checked']; 
-}
+  void toggleCheck(int index) {
+    setState(() {
+      shoppingList[index]['checked'] = !shoppingList[index]['checked'];
+    });
+  }
+
+  void removeItem(int index) {
+    setState(() {
+      shoppingList.removeAt(index);
+      controllers.removeAt(index);
+    });
+  }
+
+  @override
+  void dispose() {
+    _ghostController.dispose();
+    _ghostFocusNode.dispose();
+    super.dispose();
+  }
+
+  Widget buildItem(int index) {
+    final item = shoppingList[index];
+    final controller = controllers[index];
+
+    return Dismissible(
+      key: Key(item['item'] + index.toString()),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        color: Colors.red,
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Icon(Icons.delete, color: Colors.white),
+      ),
+      onDismissed: (direction) {
+        removeItem(index);
+      },
+      child: ListTile(
+        leading: Checkbox(
+          value: item['checked'],
+          onChanged: (_) => toggleCheck(index),
+        ),
+        title: TextField(
+          controller: controller,
+          decoration: InputDecoration(border: InputBorder.none),
+          onChanged: (value) => updateItem(index, value),
+        ),
+        trailing: Icon(Icons.drag_handle), // or leave empty
+      ),
+    );
+  }
 
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar( title: Text("Shopping List"),
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        // title: Text(widget),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      appBar: AppBar(title: Text('Shopping List')),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: shoppingList.length,
+              itemBuilder: (context, index) => buildItem(index),
             ),
-          ],
-        ),
+          ),
+          Divider(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _ghostController,
+              focusNode: _ghostFocusNode,
+              decoration: InputDecoration(
+                hintText: 'Add item...',
+                border: OutlineInputBorder(),
+              ),
+              onSubmitted: (value) {
+                addItem(value);
+                // Refocus to keep adding
+                FocusScope.of(context).requestFocus(_ghostFocusNode);
+              },
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
